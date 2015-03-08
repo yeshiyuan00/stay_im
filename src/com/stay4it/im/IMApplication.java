5da74aedbd7f99c84387e4b12fbe511182259d9f
Stay Zhang <stay.zhang@coresystems.ch>
2015-03-08 18:37:10 +0800

    create the project

---
+++ src/com/stay4it/im/IMApplication.java
@@ -0,0 +1,14 @@
+package com.stay4it.im;
+
+import android.app.Application;
+
+/** 
+ * @author Stay  
+ * @version create time：Mar 8, 2015 6:36:08 PM 
+ */
+public class IMApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
